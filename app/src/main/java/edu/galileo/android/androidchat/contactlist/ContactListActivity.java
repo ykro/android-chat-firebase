f723ce40c60c9afdec8f493af9b08438f8faf7a3
ykro <aacs85@gmail.com>
2016-01-17 09:39:24 -0600

    refactoring for butterknife usage in Fab

--- app/src/main/java/edu/galileo/android/androidchat/contactlist/ContactListActivity.java
+++ app/src/main/java/edu/galileo/android/androidchat/contactlist/ContactListActivity.java
@@ -2,19 +2,18 @@ package edu.galileo.android.androidchat.contactlist;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 import java.util.ArrayList;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import edu.galileo.android.androidchat.R;
 import edu.galileo.android.androidchat.addcontact.AddContactDialogFragment;
 import edu.galileo.android.androidchat.chat.ChatActivity;
@@ -25,7 +24,6 @@ public class ContactListActivity extends AppCompatActivity
                                  implements ContactListView, OnItemClickListener {
 
     @Bind(R.id.recyclerViewContacts) RecyclerView recyclerView;
-    @Bind(R.id.fab) FloatingActionButton fab;
     @Bind(R.id.toolbar) Toolbar toolbar;
 
     private ContactListPresenter contactListPresenter;
@@ -37,17 +35,8 @@ public class ContactListActivity extends AppCompatActivity
         setContentView(R.layout.activity_contact_list);
         ButterKnife.bind(this);
 
-        contactListPresenter = new ContactListPresenterImpl(this);
-
         setSupportActionBar(toolbar);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                AddContactDialogFragment frag = new AddContactDialogFragment();
-                frag.show(getSupportFragmentManager(), "Dialog");
-            }
-        });
-
+        contactListPresenter = new ContactListPresenterImpl(this);
 
         adapter = new ContactListAdapter(this, new ArrayList<User>(), this);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -90,10 +79,15 @@ public class ContactListActivity extends AppCompatActivity
         return super.onOptionsItemSelected(item);
     }
 
+    @OnClick(R.id.fab)
+    public void addContact(){
+        AddContactDialogFragment frag = new AddContactDialogFragment();
+        frag.show(getSupportFragmentManager(), "");
+    }
+
     @Override
     public void onContactAdded(User user) {
         adapter.add(user);
-        recyclerView.scrollToPosition(adapter.getItemCount() - 1);
     }
 
     @Override
